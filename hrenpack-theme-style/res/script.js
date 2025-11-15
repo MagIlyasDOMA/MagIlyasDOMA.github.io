da3974a8363025bd52e839091dc779e8d7aef83d
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-11-16 00:20:30 +0300

    16.11.2025 0:18
    Добавление sitemap.xml и robots.txt

--- hrenpack-theme-style/res/script.js
+++ hrenpack-theme-style/res/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const element = document.querySelector('.language-html');
-    const factory = new ClickableLinksFactory()
+    const urlRegex = /(https?:\/\/[^\s<]+)/g; // Является ли это регулярное выражение js верным: /(https?:\/\/[^\s&lt;]+)/g
+    const factory = new ClickableLinksFactory(urlRegex)
     const notification = new HyperTextNotification({backgroundColor: 'rgba(192,0,192,0.8)'})
     document.querySelector('pre').style.borderRadius = '15px';
     factory.clickToCopyLinks(element);
