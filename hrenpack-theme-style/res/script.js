d8a20d7111aeff9669ffbebefed5d62fc1ca20d1
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-11-17 17:07:27 +0300

    17.11.2025 17:06
    Начало глобального обновления hrenpack_js/install.html

--- hrenpack-theme-style/res/script.js
+++ hrenpack-theme-style/res/script.js
@@ -1,12 +1,17 @@
+const factory = new ClickableLinksFactory()
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelector('.language-html');
-    const factory = new ClickableLinksFactory()
+    const install_code_element = document.getElementById('install-div')
+        .querySelector('.language-html');
     const notification = new HyperTextNotification({backgroundColor: 'rgba(192,0,192,0.8)'})
-    document.querySelector('pre').style.borderRadius = '15px';
-    factory.clickToCopyLinks(element);
+    document.querySelectorAll('pre').forEach(pre => {
+        pre.style.borderRadius = '15px';
+    })
+    factory.clickToCopyLinks(install_code_element);
     factory.generatedElements.forEach(link => {
         link.addEventListener('click', function () {
-            notification.show("Ссылка скопирована")
+            notification.show("Ссылка скопирована");
         })
     })
 })
---
+++ hrenpack_js/res/generator.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const generator_div = document.getElementById('generator-div')
+    const code_element = generator_div.querySelector('.language-html')
+    const inputs_div = generator_div.querySelector('#generator-inputs-div')
+
+})
+
