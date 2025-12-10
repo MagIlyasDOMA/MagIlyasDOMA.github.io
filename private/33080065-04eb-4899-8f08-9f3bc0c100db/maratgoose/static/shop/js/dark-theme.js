ae90ced09ae25100b9cf7c4b61b7c395ab0c9e65
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-12-10 14:20:20 +0300

    private delete

--- private/33080065-04eb-4899-8f08-9f3bc0c100db/maratgoose/static/shop/js/dark-theme.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const tt_svg = tt_button.querySelector('img');
-const themeBaseURL = document.currentScript.dataset.toggleTheme || '/static';
-function customDarkThemeUpdate() {
-    tt_svg.src = themeBaseURL + '/' + currentTheme + '.svg';
-}
-tt_button.addEventListener('click', customDarkThemeUpdate);
-document.addEventListener('DOMContentLoaded', customDarkThemeUpdate);
-//# sourceMappingURL=dark-theme.js.map
