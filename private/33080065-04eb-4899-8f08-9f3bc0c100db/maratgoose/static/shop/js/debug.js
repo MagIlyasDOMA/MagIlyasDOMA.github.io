1d0fc303fb99cfa16d64e7d1c99914f5da2cd0a1
Маг Ильяс DOMA <157958205+MagIlyasDOMA@users.noreply.github.com>
2025-12-26 18:32:36 +0300

    Delete private directory

--- private/33080065-04eb-4899-8f08-9f3bc0c100db/maratgoose/static/shop/js/debug.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const color = stylesRoot.getPropertyValue('--header-background');
-const notWorkingNotify = new HyperTextNotification({ backgroundColor: color });
-//# sourceMappingURL=debug.js.map
